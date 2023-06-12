71950956db50f9e96eae453b4b9ad5ea9b6aff12
Mikask <darrenprasetya40@gmail.com>
2023-06-12 23:44:15 +0700

    feat: add deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'frontend',
       exec_mode: 'cluster',
-      instances: 'max',
+      instances: '2',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_production: {
